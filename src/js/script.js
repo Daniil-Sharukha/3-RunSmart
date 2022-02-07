5c451a5dd3ab2cf8e90866239d21e44fd00cbd83
damien <daniil_sharukha@icloud.com>
2022-02-08 03:59:11 +0600

    add reviews, map, tabs, validate form, modal windows,  phone mask (all adaptive)

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+//slider
 const slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -7,6 +8,7 @@ const slider = tns({
     nav: false,
     autoplayButtonOutput: false
 });
+
 document.querySelector('.prev').addEventListener('click', function() {
     slider.goTo('prev');
 });
@@ -14,11 +16,13 @@ document.querySelector('.next').addEventListener('click', function() {
     slider.goTo('next');
 });
 
+//tabs
 $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
     $(this)
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
 });
+
 
 function toggleSlide(item) {
     $(item).each(function(i) {
@@ -31,3 +35,53 @@ function toggleSlide(item) {
 }
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item__back');
+
+//modal
+$('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+});
+
+$('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
+
+$('.button__mini').each(function(i) {
+    $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+    })
+});
+
+//validate
+function validateForms(form){
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите своё имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+            },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты"
+            }
+        }
+    });
+};
+
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
+
+$('input[name=phone]').mask("+7 (999) 999-99-99")
